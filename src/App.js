1365be71191fc207780e0688e48c31c1c2bbec5e
MeryAmun <chebesiawah@gmail.com>
2022-01-05 20:19:23 +0100

    adding styles using makeStyles

--- src/App.js
+++ src/App.js
@@ -11,10 +11,21 @@ import {
   Typography,
 } from '@mui/material'
 
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import { PhotoCamera } from '@mui/icons-material'
 import React from 'react'
+import { makeStyles } from '@mui/material/styles'
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.color,
+    padding: theme.spacing(8, 0, 6),
+  },
+}))
 
 function App() {
+  const classes = useStyles()
   return (
     <>
       <CssBaseline />
@@ -25,7 +36,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <main>
-        <div>
+        <div className={classes.container}>
           <Container maxWidth='sm'>
             <Typography
               variant='h2'
@@ -35,6 +46,29 @@ function App() {
             >
               Photo Album
             </Typography>
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+              obcaecati possimus cum earum totam asperiores?
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
